bea1affb524c334a1b6490019826f3d6ab73e6a7
AmberParmaar <160378243+Amberparmaar@users.noreply.github.com>
2025-12-28 01:27:01 +0500

    Update site URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: '/',
+  url: 'https://physical-ai-humanoid-robotic-book1.vercel.app/',
   // Set the /<base>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<org-name>/'
   baseUrl: '/',
